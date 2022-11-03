3c6524efd0a85db727ecc304fa2846f75b50f52a
hiseyong <79254036+hiseyong@users.noreply.github.com>
2022-11-03 19:16:40 +0900

    design

--- src/interects/wave/WaterWave.jsx
+++ src/interects/wave/WaterWave.jsx
@@ -1,8 +1,12 @@
 import { useCanvas } from "../../hooks/useCanvas";
 import { Waves } from "./Waves";
 import { useState } from "react";
+import '../../css/Wave.css';
 export function WaterWave(props) {
     const [waveLayer, setWaveLayer] = useState(3);
+    const onChange = (e) => {
+        setWaveLayer(e.target.value);
+    }
     const wave = new Waves(props.canvasWidth, props.canvasHeight, waveLayer);
     wave.init();
     const animate = (ctx) => {
@@ -12,8 +16,7 @@ export function WaterWave(props) {
     const canvasRef = useCanvas(props.canvasWidth, props.canvasHeight, animate);
     return(
         <div>
-            
-            <input type="range" min="1" max="11" id='waverange'/>
+            <input type="range" min="1" max="11" id='waverange' onChange={onChange}/>
             <canvas ref={canvasRef} id='wavecanvas'/>
         </div>
     )
